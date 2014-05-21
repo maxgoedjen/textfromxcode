ad42bedb13b992255b6fa6adcab6eb3e13c47bef
Max Goedjen <max.goedjen@gmail.com>
2014-05-20 22:24:14 -0700

    Project setup

---
+++ TextFromXcode/MXGTextFromXcode.h
@@ -0,0 +1,13 @@
+//
+//  MXGTextFromXcode.h
+//  MXGTextFromXcode
+//
+//  Created by Max Goedjen on 5/20/14.
+//  Copyright (c) 2014 MXG. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface MXGTextFromXcode : NSObject
+
+@end
